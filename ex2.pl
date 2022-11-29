ecdb52edb0f0a0b1726efdb873edaa170e1845b1
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2022-11-29 17:38:33 -0300

    Update ex2.pl

--- ex2.pl
+++ ex2.pl
@@ -27,11 +27,14 @@ homem(jorge).
 %% X é o pai/mae e Y é o filho/filha
 pai(X,Y) :- progenitor(X,Y),sexo(X,masculino).
 mae(X,Y) :- progenitor(X,Y),sexo(X,feminino).
+pais(X,Y):-progenitor(X,Y).
 filho(Y,X) :- progenitor(X,Y),sexo(Y,masculino).
 filha(Y,X) :- progenitor(X,Y),sexo(Y,feminino).
 filho_a_(Y,X):-progenitor(X,Y).
 
+%%% X irma e Y é´o irmao 
 irma(X,Y):-progenitor(A,X),progenitor(A,Y),X\==Y , sexo(X,feminino).
+%% X irmao e Y é irma
 irmao(X,Y):-progenitor(A,X),progenitor(A,Y),X\==Y , sexo(X,masculino).
 
 descendente(X,Y):-progenitor(X,Y).
@@ -40,6 +43,10 @@ descendente(X,Y):-progenitor(X,Y).
 
 vô(X,Y):-progenitor(X,A),progenitor(A,Y),sexo(X,masculino).
 vó(X,Y):-progenitor(X,A),progenitor(A,Y),sexo(X,feminino).
+avós(X,Y):-progenitor(X,A),progenitor(A,Y).
+avós_paternos(X,Y):-progenitor(X,A),sexo(A,masculino),progenitor(A,Y).
+avós_maternos(X,Y):-progenitor(X,A),sexo(A,feminino),progenitor(A,Y).
+
 %% consulta que mostrar os netos 
 neto/a(X,Y):-filho_a_(X,A),filho_a_(A,Y).
 
@@ -52,6 +59,11 @@ tia(X,Y):-irma(X,A),progenitor(A,Y).
 sobrinho(X,Y):-irma(Y,A),filho(X,A).
 
 sobrinha(X,Y):-irma(Y,A),filha(X,A).
+
+%%% X é  ascendente  e Y é os pais 
+ascendente(X,Y):-filho_a_(X,Y).
+
+
 
 
 
