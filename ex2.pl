fcd9b32913e150c05a44b3182c51f0e11bc12304
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2022-11-29 16:14:46 -0300

    Update ex2.pl

--- ex2.pl
+++ ex2.pl
@@ -1,3 +1,4 @@
+
 progenitor(maria, jose).
 progenitor(joao, jose).
 progenitor(joao, ana).
@@ -9,12 +10,49 @@ mulher(maria).
 mulher(ana).
 mulher(julia).
 mulher(iris).
+
+sexo(maria,feminino).
+sexo(ana,feminino).
+sexo(julia,feminino).
+sexo(iris,feminino).
+sexo(jose,masculino).
+sexo(joao,masculino).
+sexo(jorge,masculino).
 homem(jose).
 homem(joao).
 homem(jorge).
 
-prole(Y,X) :- progenitor(X,Y).
-mae(X,Y) :- progenitor(X,Y), mae(X).
-avos(X,Y):- progenitor(X,Y), mulher(X).
-descendente(X,Z) :- progenitor(X,Z).
-descendente(X,Z) :- progenitor(X,Z), descendente(Y,Z).
+
+
+%% X é o pai/mae e Y é o filho/filha
+pai(X,Y) :- progenitor(X,Y),sexo(X,masculino).
+mae(X,Y) :- progenitor(X,Y),sexo(X,feminino).
+filho(Y,X) :- progenitor(X,Y),sexo(Y,masculino).
+filha(Y,X) :- progenitor(X,Y),sexo(Y,feminino).
+filho_a_(Y,X):-progenitor(X,Y).
+
+irma(X,Y):-progenitor(A,X),progenitor(A,Y),X\==Y , sexo(X,feminino).
+irmao(X,Y):-progenitor(A,X),progenitor(A,Y),X\==Y , sexo(X,masculino).
+
+descendente(X,Y):-progenitor(X,Y).
+%descendente(X,Y):-progenitor(X,Y),descendente(A,Y).
+
+
+vô(X,Y):-progenitor(X,A),progenitor(A,Y),sexo(X,masculino).
+vó(X,Y):-progenitor(X,A),progenitor(A,Y),sexo(X,feminino).
+%% consulta que mostrar os netos 
+neto/a(X,Y):-filho_a_(X,A),filho_a_(A,Y).
+
+
+%% X é o tio/tia e Y e o sobrinho logo
+tio(X,Y):-irmao(X,A),progenitor(A,Y).
+tia(X,Y):-irma(X,A),progenitor(A,Y).
+
+%% X é o sobrinho e  Y é a tia  
+sobrinho(X,Y):-irma(Y,A),filho(X,A).
+
+sobrinha(X,Y):-irma(Y,A),filha(X,A).
+
+
+
+
